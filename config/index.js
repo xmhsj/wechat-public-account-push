855c2f5ee365c37c991c39550f0e61019b6366cd
xmhsj <56779046+xmhsj@users.noreply.github.com>
2022-09-05 17:26:30 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -37,7 +37,12 @@ export const config = {
       // 所在城市
       city: "长沙",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://wangxinleo.cn"
+      openUrl: "https://wangxinleo.cn",
+      title: "下午好呀呀鸭",
+      constellation: "金牛座",
+      love: "99",
+      color: "五颜六色",
+      state："喜滋滋"
     }, 
   ],
 
